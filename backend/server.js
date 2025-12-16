6e37c23e8b763a7c29c0f148d87ae7d341e863b1
Jaisbabu <jaisbabu2001@gmail.com>
2025-12-16 21:14:45 +0530

    Fix CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -43,12 +43,37 @@ app.use(
 );
 
 /* -------------------- CORS -------------------- */
+
+  const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://ai-image-gallery-sigma.vercel.app'
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: function (origin, callback) {
+      // allow requests with no origin (Postman, server-to-server)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'));
+    },
     credentials: true
   })
 );
+
+
+
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || 'https://ai-image-gallery-sigma.vercel.app',
+//     credentials: true
+//   })
+// );
 
 /* -------------------- BODY PARSING -------------------- */
 app.use(express.json());
